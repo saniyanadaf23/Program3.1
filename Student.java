a7e3cdf3aa4c8883bcd67aa74471c47907166fed
qanusha436 <amoghajoshi2005@gmail.com>
2025-05-06 14:57:04 +0530

    main

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    String name;
+    int id;
+    String course;
+    int phone;
+    public Student(String name,int id,String course,int phone){
+        this.name = name;
+        this.id = id;
+        this.course = course;
+        this.phone = phone;
+    }
+    public void displayInfo(){
+        System.out.println("Student Name:"+name);
+        System.out.println("Student ID:"+id);
+        System.out.println("Course:"+course);
+        System.out.println("Phone Number:"+phone);
+    }
+    public static void main(String[] args) {
+       Student stu1 = new Student("saniya",101,"BCA",891766353);
+        stu1.displayInfo();
+    }
+}
